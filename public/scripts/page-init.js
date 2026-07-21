55e93dce463b1b22cacbfec05b170375c508e67b
Ahmed Rehan <ar27111994@gmail.com>
2026-07-21 14:52:25 +0500

    fix: autoHeight true for case studies Swiper

--- public/scripts/page-init.js
+++ public/scripts/page-init.js
@@ -350,6 +350,7 @@ function initPage() {
       new Swiper("#case-studies-swiper", {
         slidesPerView: 1,
         spaceBetween: 16,
+        autoHeight: true,
         autoplay: { delay: 6000, pauseOnMouseEnter: true, disableOnInteraction: false },
         pagination: { el: ".swiper-pagination", clickable: true },
         navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
